1c9f4dc3016a48c99262f42bba5f7b82f0710ae3
Taka Wang <wang.chia.ming@gmail.com>
2016-09-12 14:38:45 +0800

    Refine main

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	route "github.com/taka-wang/mb-web/route"
 	worker "github.com/taka-wang/mb-web/worker"
 )
 
 func main() {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		// start cleanup
+		worker.Stop()
+		// stop cleanup
+		os.Exit(0)
+	}()
+
 	worker.Start()
 	// link worker and route packages
 	route.SetRequestHandler(worker.RequestHandler)
